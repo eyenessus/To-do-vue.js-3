daf49b992ac0900c438b8e8b070eab972ecf3b7a
Emerson S <eyenessus>
2023-12-23 10:23:18 -0300

    ::Adicionado meu primeiro componente::

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{}>()
+</script>
+
+<template>
+    <header class="header">
+        UMA HEADER
+    </header>
+</template>
+
+<style scoped>
+.header{
+    background-color: blue;
+}
+</style>
